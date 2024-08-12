f8b9becdb8a6d4f0958bad8b3388e066fbadcf89
S4NKALP <sankalptharu50028@gmail.com>
2024-08-13 02:42:59 +0545

    .

--- init.lua
+++
@@ -1,7 +0,0 @@
-require('core.options')
-require('core.functions')
-require('core.keys')
-require('core.autocmd')
-require('plugins.lazy')
--- Add user configs to this module
-pcall(require, 'user')
