cff80541c9d7c18766392af5f93f1a287885c4da
S4NKALP <sankalptharu50028@gmail.com>
2024-08-13 02:42:06 +0545

    .

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,5 +1,5 @@
 local function augroup(name)
-    return vim.api.nvim_create_augroup('nvim2k_' .. name, { clear = true })
+    return vim.api.nvim_create_augroup('nvim' .. name, { clear = true })
 end
 
 -- Strip trailing spaces before write
