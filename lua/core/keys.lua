61eb9d72c06442f70476f49f99a86582f695de93
S4NKALP <sankalptharu50028@gmail.com>
2025-06-02 15:42:42 +0545

    refactor(normal, visual): add VSCode-style shortcuts and key mappings for various functions

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -14,32 +14,70 @@ vim.g.maplocalleader = ' '
 -- normal_mode = "n", insert_mode = "i", visual_mode = "v",
 -- visual_block_mode = "x", term_mode = "t", command_mode = "c",
 
--- Comment
--- map({ 'n', 'v' }, '<leader>;', 'gcc', { desc = 'Comment toggle', remap = true })
--- map({ 'v', 'x' }, '<leader>;', 'gc', { desc = 'Comment toggle', remap = true })
-
--- Able to use semicolon in normal mode
-map('n', ';', ':', { desc = '; Command mode' })
-
--- Select all
-map('n', '<C-a>', 'gg<S-v>G')
-
 -- Use jj as escape
 map('i', 'jj', '<Esc>', opts)
-map('i', 'jk', '<Esc>', opts)
-map('i', 'kj', '<Esc>', opts)
 map('t', 'JJ', '<C-\\><C-n>', opts)
 
--- Terminal Mappings
-map('t', 'jk', '<C-\\><C-n>')
-map('t', 'kj', '<C-\\><C-n>')
-map('t', '<ESC>', '<C-\\><C-n>')
+-- VSCode style shortcuts
+-- File operations
+map('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
+map('i', '<C-s>', '<Esc>:w<CR>a', { desc = 'Save file' })
+map('n', '<C-q>', ':q<CR>', { desc = 'Quit' })
+map('n', '<C-w>', ':bd<CR>', { desc = 'Close buffer' })
+
+-- Undo/Redo
+map('n', '<C-z>', 'u', { desc = 'Undo' })
+map('i', '<C-z>', '<C-o>u', { desc = 'Undo' })
+map('n', '<C-y>', '<C-r>', { desc = 'Redo' })
+map('i', '<C-y>', '<C-o><C-r>', { desc = 'Redo' })
+
+-- Select all
+map('n', '<C-a>', 'ggVG', { desc = 'Select all' })
+map('i', '<C-a>', '<Esc>ggVG', { desc = 'Select all' })
+
+-- Copy/Cut
+map('v', '<C-c>', 'y', { desc = 'Copy' })
+map('v', '<C-x>', 'd', { desc = 'Cut' })
+
+-- Line operations (VSCode style)
+map('n', '<A-Up>', ':m .-2<CR>==', { desc = 'Move line up' })
+map('n', '<A-Down>', ':m .+1<CR>==', { desc = 'Move line down' })
+map('i', '<A-Up>', '<Esc>:m .-2<CR>==gi', { desc = 'Move line up' })
+map('i', '<A-Down>', '<Esc>:m .+1<CR>==gi', { desc = 'Move line down' })
+map('v', '<A-Up>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+map('v', '<A-Down>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+
+-- VSCode-style line operations
+map('n', '<S-CR>', 'O<Esc>', { desc = 'Insert line above' })
+map('n', '<CR>', 'o<Esc>', { desc = 'Insert line below' })
+map('i', '<S-CR>', '<C-o>O', { desc = 'Insert line above' })
+
+-- Quick word operations
+map('n', '<C-BS>', 'db', { desc = 'Delete word backward' })
+map('i', '<C-BS>', '<C-w>', { desc = 'Delete word backward' })
+map('n', '<C-Del>', 'dw', { desc = 'Delete word forward' })
+map('i', '<C-Del>', '<C-o>dw', { desc = 'Delete word forward' })
+
+-- Duplicate line
+map('n', '<A-S-Up>', 'yykP', { desc = 'Duplicate line up' })
+map('n', '<A-S-Down>', 'yyp', { desc = 'Duplicate line down' })
+map('v', '<A-S-Up>', "y'<Pgv", { desc = 'Duplicate selection up' })
+map('v', '<A-S-Down>', "y'>pgv", { desc = 'Duplicate selection down' })
+
+
+-- Line manipulation
+map('n', '<C-]>', '>>', { desc = 'Indent line' })
+map('n', '<C-[>', '<<', { desc = 'Unindent line' })
+map('i', '<C-]>', '<C-o>>>', { desc = 'Indent line' })
+map('i', '<C-[>', '<C-o><<', { desc = 'Unindent line' })
 
 -- Visual overwrite paste
 map({ 'v', 'x' }, 'p', '"_dP', opts)
 
 -- Do not copy on x
 map({ 'v', 'x' }, 'x', '"_x', opts)
+map('n', 'X', '"_D', opts)
+map({ 'v', 'x' }, 'X', '"_d', opts)
 
 -- Increment/decrement
 map({ 'n', 'v', 'x' }, '-', '<C-x>', opts)
@@ -67,12 +105,6 @@ map({ 'v', 'x' }, 'K', ":move '<-2<cr>gv-gv", opts)
 -- Clear search, diff update and redraw
 map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
--- Cursor navigation on insert mode
-map('i', '<M-h>', '<left>', { desc = 'Move Cursor Left' })
-map('i', '<M-l>', '<right>', { desc = 'Move Cursor Left' })
-map('i', '<M-j>', '<down>', { desc = 'Move Cursor Left' })
-map('i', '<M-k>', '<up>', { desc = 'Move Cursor Left' })
-
 -- Consistent n/N search navigation
 map('n', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
 map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
@@ -90,25 +122,7 @@ map('i', ',', ',<c-g>u')
 map('i', '.', '.<c-g>u')
 map('i', ';', ';<c-g>u')
 
--- Move Lines
-map('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move lines', silent = true })
-map('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move lines', silent = true })
-map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Move lines', silent = true })
-map('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move lines', silent = true })
-map('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move lines', silent = true })
-map('i', '<A-k>', '<esc>:m .-2<CR>==gi', { desc = 'Move lines', silent = true })
 
-map('n', 'd', '"_d', { desc = 'Delete without replacing clipboard' })
-map('n', 'x', '"_x', { desc = 'Cut without replacing clipboard' })
-map('n', 'c', '"_c', { desc = 'Change without replacing clipboard' })
-map('v', 'p', '"_dP', { desc = 'Paste without replacing clipboard' })
 
--- Toggle relative numbers off or on (for code presentation)
-map({ 'n', 'v' }, '<leader>tn', '<cmd>set rnu!<cr>', {})
 
---insert the following characters around the visual selection
-map('v', '<leader>(', 's()<esc>Pll', { desc = 'wrap selection with parens' })
-map('v', '<leader>[', 's[]<esc>Pll', { desc = 'wrap selection with square braces' })
-map('v', '<leader>{', 's{}<esc>Pll', { desc = 'wrap selection with curly braces' })
-map('v', "<leader>'", "s''<esc>Pll", { desc = 'wrap selection with single quotes' })
-map('v', '<leader>"', 's""<esc>Pll', { desc = 'wrap selection with double quotes' })
+
